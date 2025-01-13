67b908ca8115abfce47c42b866b379693c537236
Nicolas Toffolo <toffolo01@hotmail.com>
2025-01-13 21:58:31 +0100

    docs(sql): add sensor_checkins table creation

--- sql/sensor_data/create_table.sql
+++ sql/sensor_data/create_table.sql
@@ -5,3 +5,8 @@ CREATE TABLE sensor_data (
     value INTEGER, -- For a temperature, this should be in Celsius, rounded to nearest .5 and multiplied by 10
     PRIMARY KEY (timestamp, location, type)
 );
+
+CREATE TABLE sensor_checkins (
+    sensor_id INTEGER PRIMARY KEY,
+    last_timestamp INTEGER DEFAULT (strftime('%s', 'now'))
+);
